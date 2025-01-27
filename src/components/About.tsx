29fca32402c67d8de05406e8d330d6ba20278177
almajulioo <alma.122140057@student.itera.ac.id>
2025-01-27 19:14:31 +0700

    add about card

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,20 +3,19 @@ import { Paragraph } from '@/components/Paragraph';
 import Image from 'next/image';
 
 import { motion } from 'framer-motion';
+import sukamarga from 'public/images/sukamargahome-about.png';
+import sukamargaumkm from 'public/images/sukamargaumkm-about.png';
+import whickerbutler from 'public/images/whickerbutler-abouts.png';
+import whickerbutler2 from 'public/images/whickerbutler2-about.png';
 
 export default function About() {
-    const images = [
-        'https://images.unsplash.com/photo-1692544350322-ac70cfd63614?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=800&q=60',
-        'https://images.unsplash.com/photo-1692374227159-2d3592f274c9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw4fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=800&q=60',
-        'https://images.unsplash.com/photo-1692005561659-cdba32d1e4a1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxOHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60',
-        'https://images.unsplash.com/photo-1692445381633-7999ebc03730?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzM3x8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60',
-    ];
+    const images = [sukamarga, whickerbutler2, sukamargaumkm, whickerbutler];
     return (
         <div>
-            {/* <div className="grid grid-cols-2 md:grid-cols-4 gap-10 my-10">
+            <div className="grid grid-cols-2 md:grid-cols-4 gap-10 my-10">
                 {images.map((image, index) => (
                     <motion.div
-                        key={image}
+                        key={image.src}
                         initial={{
                             opacity: 0,
                             y: -50,
@@ -37,7 +36,7 @@ export default function About() {
                         />
                     </motion.div>
                 ))}
-            </div> */}
+            </div>
 
             <div className="max-w-4xl">
                 <Paragraph className="mt-4">
